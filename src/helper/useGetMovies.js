4f12d5e2b8fd5091632cef4bb0ccb1d09a36ccc6
MiguelCM <jmiguelcardona.moriano@gmail.com>
2023-10-05 14:42:47 -0500

    Agregando variable de entorno

--- src/helper/useGetMovies.js
+++ src/helper/useGetMovies.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 
 export const useGetMovies = () => {
   const urlBase = "https://api.themoviedb.org/3/search/movie";
-  const API_KEY = "60d5dd1e3738a359b4042dc9d4aebc39";
+  const API_KEY = import.meta.env.VITE_APIKEY;
+
+console.log(import.meta.VITE_APIKEY)
 
   const [search, setSearch] = useState("");
   const [peliculas, setPeliculas] = useState([]);
